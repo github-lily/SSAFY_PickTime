06186100c71a8c89a433d6da64345c3daebd4917
99minj0731 <99minj0731@naver.com>
2025-03-22 21:14:52 +0900

    Feat: 회원정보페이지 유저 정보 조회 UI 수정 및 프로필 사진 변경 S12P21B101-137

--- FRONTEND/app/src/main/java/com/example/picktimeapp/ui/mypage/MyPageScreen.kt
+++ FRONTEND/app/src/main/java/com/example/picktimeapp/ui/mypage/MyPageScreen.kt
@@ -1,13 +1,22 @@
 package com.example.picktimeapp.ui.mypage
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,6 +29,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.picktimeapp.R
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun MyPageScreen(viewModel: MyPageViewModel) {
@@ -29,67 +40,87 @@ fun MyPageScreen(viewModel: MyPageViewModel) {
     val userInfo by viewModel.userInfo.collectAsState()
     val pickDayData by viewModel.pickDayData.collectAsState()
 
-    Row (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp)
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color =  Color(0xFFFFFDF8)
     ) {
-        //Painter - 이미지를 화면에 그릴 준비를 하는 타입
-        val profileImage: Painter  = painterResource(
-            id = when (userInfo?.level) {
-                1 -> R.drawable.profile_level_1
-                2 -> R.drawable.profile_level_2
-                3 -> R.drawable.profile_level_3
-                4 -> R.drawable.profile_level_4
-                5 -> R.drawable.profile_level_5
-                6 -> R.drawable.profile_level_6
-                else -> R.drawable.profile_level_1
-
-            }
-
-        )
-        Image(
-            painter = profileImage,
-            contentDescription = "Profile Image",
+        Column (
             modifier = Modifier
-                .size(150.dp)
-                .padding(end = 24.dp),
-            // contentScale.Crop - 지정한 사이즈에서 넘치면 자르기
-            contentScale = ContentScale.Crop
-        )
-
-
-        Column(
-            // Row 안에서 세로 방향 가운데 정렬
-            modifier = Modifier
-                .align(Alignment.CenterVertically)
+                .fillMaxSize()
+                .padding(24.dp)
         ){
-            Text(
-                text = "피크타임",
-                style = MaterialTheme.typography.titleLarge
-            )
-            Text(
-                text = "${pickDayData?.continued ?: 30}일 연속 유지중",
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(top = 4.dp)
-            )
-            // 유저 정보 출력하기
-            userInfo?.let {
-                Text(
-                    text = it.name,
-                    style = MaterialTheme.typography.titleSmall,
-                    modifier = Modifier.padding(top = 4.dp)
-                )
-            }
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(160.dp)
+                        .background(color = Color.White, shape = CircleShape)
+                        .border(width = 4.dp, color = Color(0xFFA57145), shape = CircleShape),
+                    contentAlignment = Alignment.Center
+                ){
+                    //Painter - 이미지를 화면에 그릴 준비를 하는 타입
+                    val profileImage: Painter  = painterResource(
+                        id = when (userInfo?.level) {
+                            1 -> R.drawable.profile_level_1
+                            2 -> R.drawable.profile_level_2
+                            3 -> R.drawable.profile_level_3
+                            4 -> R.drawable.profile_level_4
+                            5 -> R.drawable.profile_level_5
+                            6 -> R.drawable.profile_level_6
+                            else -> R.drawable.profile_level_1
+                        }
+                    )
+                    Image(
+                        painter = profileImage,
+                        contentDescription = "Profile Image",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(CircleShape),
+                        // contentScale.Crop - 지정한 사이즈에서 넘치면 자르기
+                        contentScale = ContentScale.Crop
+                    )
 
-            Button(
-                onClick = {},
-                modifier = Modifier.padding(top = 16.dp)
-            ){
-                Text(text = "회원정보 수정")
-            }
+                }
+                Spacer(modifier = Modifier.width(24.dp))
 
+                Column(
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "피크타임",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Text(
+                        text = "${pickDayData?.continued ?: 30}일 연속 유지중",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                    // 유저 정보 출력하기
+                    userInfo?.let {
+                        Text(
+                            text = it.name,
+                            style = MaterialTheme.typography.titleSmall,
+                            modifier = Modifier.padding(top = 4.dp)
+                        )
+                    }
+
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0x33E0CDA8),
+                            contentColor = Color(0xFF66656C)
+                        ),
+                        modifier = Modifier.padding(top = 16.dp)
+                    ){
+                        Text(text = "회원정보 수정")
+                    }
+                }
         }
+    }
+
 
 
     }
