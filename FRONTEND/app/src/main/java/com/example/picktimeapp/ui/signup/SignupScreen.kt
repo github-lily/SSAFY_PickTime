7b8f3470650f15763da92116e73d26a941552a15
hyunhee <jhc03032@naver.com>
2025-03-25 00:01:49 +0900

    Feat : SignUpPage 퍼블리싱 - signup api도 살짝 만ã듦 - S12P21B101-172

--- FRONTEND/app/src/main/java/com/example/picktimeapp/ui/signup/SignupScreen.kt
+++ FRONTEND/app/src/main/java/com/example/picktimeapp/ui/signup/SignupScreen.kt
@@ -3,9 +3,380 @@ package com.example.picktimeapp.ui.signup
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.text.font.FontWeight
+import com.example.picktimeapp.ui.theme.*
+import com.example.picktimeapp.R
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import com.example.picktimeapp.ui.signup.SignupViewModel
+
+
+
 @Composable
 fun SignupScreen(
-    modifier: Modifier = Modifier,
+    viewModel: SignupViewModel,
+    onLoginClick: () -> Unit,
+    onSignUpClick: () -> Unit
 ) {
+    // 상태 관리
+    val name = viewModel.name.value
+    val email = viewModel.email.value
+    val password = viewModel.password.value
+    val passwordCheck = viewModel.passwordcheck.value
+    val isPasswordVisible = viewModel.isPasswordVisible.value
+    val isPasswordCheckVisible = viewModel.isPasswordCheckVisible.value
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // 로고
+        Text(
+            text = "피크 타임",
+            fontFamily = com.example.picktimeapp.ui.TitleFont,
+            fontWeight = FontWeight.Medium,
+            fontSize = 70.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // 👉 닉네임 입력창
+        NameInputField(
+            name = name,
+            onNameChange = { viewModel.onNameChanged(it) }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // 👉 이메일 입력창
+        EmailInputField(
+            email = email,
+            onEmailChange = { viewModel.onEmailChanged(it) }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // 👉 비밀번호 입력창
+        PasswordInputField(
+            password = password,
+            onPasswordChange = { viewModel.onPasswordChanged(it) },
+            isPasswordVisible = isPasswordVisible,
+            onVisibilityToggle = { viewModel.togglePasswordVisibility() }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // 👉 비밀번호 확인 입력창
+        PasswordCheckInputField(
+            passwordCheck = passwordCheck,
+            onPasswordCheckChange = { viewModel.onPasswordCheckChanged(it) },
+            isPasswordCheckVisible = isPasswordCheckVisible,
+            onVisibilityToggle = { viewModel.togglePasswordCheckVisibility() }
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        // 👉 회원가입 버튼
+        SignUpButton(
+            onClick = {
+                viewModel.signup()
+                onSignUpClick() // 성공 시 다음 화면으로 전환하고 싶다면 여기에 조건 추가
+            },
+            enabled = name.isNotBlank() && email.isNotBlank() && password.isNotBlank() && passwordCheck.isNotBlank()
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        // 👉 하단 텍스트 버튼
+        LoginFooterButtons(
+            onLoginClick = onLoginClick
+        )
+    }
 }
+
+
+// ✅ Composable 함수
+
+@Composable
+fun NameInputField(
+    name: String,
+    onNameChange: (String) -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth(0.4f)) {
+        Text(
+            text = "닉네임",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Gray70
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            textStyle = MaterialTheme.typography.bodySmall,
+            singleLine = true,
+            shape = RoundedCornerShape(12.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Gray30,
+                focusedBorderColor = Brown40,
+                cursorColor = Brown40,
+                unfocusedTextColor = Gray90,
+                focusedTextColor = Gray90,
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White
+            ),
+            placeholder = {
+                Text(
+                    text = "기타둥둥기린이",
+                    color = Gray50,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = { onNameChange("") }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_clear),
+                        contentDescription = "Clear Name",
+                        tint = Gray50
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun EmailInputField(
+    email: String,
+    onEmailChange: (String) -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth(0.4f)) {
+        Text(
+            text = "이메일",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Gray70
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = onEmailChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            textStyle = MaterialTheme.typography.bodySmall,
+            singleLine = true,
+            shape = RoundedCornerShape(12.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Gray30,
+                focusedBorderColor = Brown40,
+                cursorColor = Brown40,
+                unfocusedTextColor = Gray90,
+                focusedTextColor = Gray90,
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White
+            ),
+            placeholder = {
+                Text(
+                    text = "picktime@guitar.com",
+                    color = Gray50,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = { onEmailChange("") }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_clear), // ❗ X 버튼 아이콘 준비 필요
+                        contentDescription = "Clear Email",
+                        tint = Gray50
+                    )
+                }
+            }
+        )
+    }
+}
+
+
+@Composable
+fun PasswordInputField(
+    password: String,
+    onPasswordChange: (String) -> Unit,
+    isPasswordVisible: Boolean,
+    onVisibilityToggle: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth(0.4f)) {
+        Text(
+            text = "비밀번호",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Gray70
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = onPasswordChange,
+            textStyle = MaterialTheme.typography.bodySmall,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            singleLine = true,
+            shape = RoundedCornerShape(12.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Gray30,
+                focusedBorderColor = Brown40,
+                cursorColor = Brown40,
+                unfocusedTextColor = Gray90,
+                focusedTextColor = Gray90,
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White
+            ),
+            placeholder = {
+                Text(
+                    text = "비밀번호를 입력하세요",
+                    color = Gray50,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = onVisibilityToggle) {
+                    Icon(
+                        imageVector = if (isPasswordVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                        contentDescription = if (isPasswordVisible) "비밀번호 숨기기" else "비밀번호 보기",
+                        tint = Gray50
+                    )
+                }
+            }
+        )
+    }
+}
+
+
+@Composable
+fun PasswordCheckInputField(
+    passwordCheck: String,
+    onPasswordCheckChange: (String) -> Unit,
+    isPasswordCheckVisible: Boolean,
+    onVisibilityToggle: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth(0.4f)) {
+        Text(
+            text = "비밀번호 확인",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Gray70
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(
+            value = passwordCheck,
+            onValueChange = onPasswordCheckChange,
+            textStyle = MaterialTheme.typography.bodySmall,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            singleLine = true,
+            shape = RoundedCornerShape(12.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = if (isPasswordCheckVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Gray30,
+                focusedBorderColor = Brown40,
+                cursorColor = Brown40,
+                unfocusedTextColor = Gray90,
+                focusedTextColor = Gray90,
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White
+            ),
+            placeholder = {
+                Text(
+                    text = "비밀번호를 한번 더 입력하세요",
+                    color = Gray50,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = onVisibilityToggle) {
+                    Icon(
+                        imageVector = if (isPasswordCheckVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                        contentDescription = if (isPasswordCheckVisible) "비밀번호 숨기기" else "비밀번호 보기",
+                        tint = Gray50
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun SignUpButton(
+    onClick: () -> Unit,
+    enabled: Boolean = true
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth(0.4f)
+            .padding(horizontal = 20.dp)
+            .height(80.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Brown60,
+            contentColor = DarkGreen10,
+            disabledContainerColor = Brown40,
+            disabledContentColor = Gray50
+        ),
+        shape = RoundedCornerShape(12.dp),
+        enabled = enabled
+    ) {
+        Text(
+            text = "회원가입",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Gray10,
+            fontWeight = FontWeight.Medium
+
+        )
+    }
+}
+
+@Composable
+fun LoginFooterButtons(
+    onLoginClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        TextButton(onClick = onLoginClick) {
+            Text(
+                text = "이미 계정이 있으신가요?",
+                style = MaterialTheme.typography.bodySmall,
+                color = Gray70
+            )
+        }
+    }
+}
+
--- FRONTEND/app/src/main/java/com/example/picktimeapp/ui/signup/SignupViewModel.kt
+++ FRONTEND/app/src/main/java/com/example/picktimeapp/ui/signup/SignupViewModel.kt
@@ -1,4 +1,88 @@
 package com.example.picktimeapp.ui.signup
 
-class SignupViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.picktimeapp.network.SignUpApi
+import com.example.picktimeapp.network.SignUpRequest
+import kotlinx.coroutines.launch
+import androidx.lifecycle.viewModelScope
+import androidx.compose.runtime.mutableStateOf
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import android.util.Log
+import retrofit2.Response
+
+
+@HiltViewModel
+class SignupViewModel @Inject constructor(private val signUpApi : SignUpApi) : ViewModel() {
+
+    // 입력값 상태관리
+    var name = mutableStateOf("")
+        private set
+
+    var email = mutableStateOf("")
+        private set
+
+    var password = mutableStateOf("")
+        private set
+
+    var passwordcheck = mutableStateOf("")
+        private set
+
+    // 비밀번호 암호화 설정
+    var isPasswordVisible = mutableStateOf(false)
+        private set
+
+    var isPasswordCheckVisible = mutableStateOf(false)
+        private set
+
+    // 변경 감지
+    fun onNameChanged(newName: String) {
+        name.value = newName
+
+    }
+
+    fun onEmailChanged(newEmail: String) {
+        email.value = newEmail
+    }
+
+    fun onPasswordChanged(newPassword: String) {
+        password.value = newPassword
+    }
+
+    fun onPasswordCheckChanged(newPasswordCheck: String) {
+        passwordcheck.value = newPasswordCheck
+    }
+
+    // 비밀번호 암호화 토글
+    fun togglePasswordVisibility() {
+        isPasswordVisible.value = !isPasswordVisible.value
+    }
+
+    fun togglePasswordCheckVisibility() {
+        isPasswordCheckVisible.value = !isPasswordCheckVisible.value
+    }
+
+
+    var signUpResult = mutableStateOf<Response<Unit>?>(null)
+    var errorMessage = mutableStateOf<String?>(null)
+
+    fun signup() {
+        viewModelScope.launch {
+            try {
+                val response = signUpApi.login(
+                    SignUpRequest(
+                        name = name.value,
+                        username = email.value,
+                        password = password.value
+                    )
+                )
+                signUpResult.value = response
+                Log.d("SignUpViewModel", "회원가입 성공")
+            } catch (e: Exception) {
+                errorMessage.value = "회원가입 실패: ${e.message}"
+                Log.e("SignUpViewModel", "회원가입 실패", e)
+            }
+        }
+    }
 }
+
