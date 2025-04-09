746b2a6610cfdbb070251bf2abf1e7a8bff13e38
이병조 <qudwh0312@naver.com>
2025-04-09 22:51:13 +0900

    Fix : FingerDetectionResponse @SerializedName으로 네이밍 매핑

--- FRONTEND/app/src/main/java/com/example/picktimeapp/data/model/ChordDetectionResult.kt
+++ FRONTEND/app/src/main/java/com/example/picktimeapp/data/model/ChordDetectionResult.kt
@@ -1,7 +1,12 @@
 package com.example.picktimeapp.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class FingerDetectionResponse(
+    @SerializedName("detection_done")
     val detectionDone: Boolean,
+
+    @SerializedName("finger_positions")
     val fingerPositions: Map<String, FingerPositionData>
 )
 
