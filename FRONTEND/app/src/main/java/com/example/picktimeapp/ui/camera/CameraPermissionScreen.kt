0c434299ecd5d9b2b09c91c96e83631076364b2b
이병조 <qudwh0312@naver.com>
2025-04-10 14:32:08 +0900

    Fix : 카메라 및 마이크 권한 허용

--- FRONTEND/app/src/main/java/com/example/picktimeapp/ui/camera/CameraPermissionScreen.kt
+++ FRONTEND/app/src/main/java/com/example/picktimeapp/ui/camera/CameraPermissionScreen.kt
@@ -22,26 +22,38 @@ fun CameraPermissionScreen(
     var showRationale by remember { mutableStateOf(false) }
 
     val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission(),
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
         onResult = { isGranted ->
-            hasPermission = isGranted
-            if (!isGranted) showRationale = true
+
+            val cameraGranted = isGranted[Manifest.permission.CAMERA] == true
+            val micGranted = isGranted[Manifest.permission.RECORD_AUDIO] == true
+
+            hasPermission = cameraGranted && micGranted
+            if (!hasPermission) showRationale = true
         }
     )
 
     LaunchedEffect(Unit) {
-        when {
-            ContextCompat.checkSelfPermission(
-                context,
-                Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                hasPermission = true
-            }
-            else -> {
-                launcher.launch(Manifest.permission.CAMERA)
-            }
+        val cameraCheck = ContextCompat.checkSelfPermission(
+            context, Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val micCheck = ContextCompat.checkSelfPermission(
+            context, Manifest.permission.RECORD_AUDIO
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (cameraCheck && micCheck) {
+            hasPermission = true
+        } else {
+            launcher.launch(
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO
+                )
+            )
         }
     }
+
 
     when {
         hasPermission -> onPermissionGranted()
@@ -50,7 +62,7 @@ fun CameraPermissionScreen(
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ){
-                Text("카메라 권한이 필요합니다. 설정 > 권한에서 허용해주세요.")
+                Text("카메라 및 마이크 권한이 필요합니다. 설정 > 권한에서 허용해주세요.")
             }
         }
     }
